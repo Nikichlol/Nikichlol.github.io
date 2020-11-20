3773f995362154e1ee1f8aa11d16e1ca7741121d
Nikichlol <awtolmolog@mail.ru>
2020-11-20 08:38:13 +0300

    qwe

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,43 +1,44 @@
-let head = document.querySelector('h1');
-head.textContent = 'Я заменил это js-ом'
+const head = document.querySelector('h1');
+head.textContent = 'Я заменил это js-ом';
 
 document.querySelector('h1').onclick = function () {
-    alert('Ouch! Stop poking me!');
-}
+   alert('Ouch! Stop poking me!');
+};
+
 
 function setUserName() {
-    var myName = prompt('Please enter your name. And');
-    localStorage.setItem('name', myName);
-    head.textContent = `Приветствую на строничке гента, ${myName}`;
-  }
+   const myName = prompt('Please enter your name. And');
+   localStorage.setItem('name', myName);
+   head.textContent = `Приветствую на строничке гента, ${myName}`;
+}
 myButton.onclick = function() {
-    setUserName();
-  }
+   setUserName();
+};
 
-var myButton = document.querySelector('button');
-myButton.onclick = setUserName
-  if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    var storedName = localStorage.getItem('name');
-    head.textContent = `Приветствую на строничке гента, ${storedName}`;
-  }
+const myButton = document.querySelector('button');
+myButton.onclick = setUserName;
+if (!localStorage.getItem('name')) {
+   setUserName();
+} else {
+   const storedName = localStorage.getItem('name');
+   head.textContent = `Приветствую на строничке гента, ${storedName}`;
+}
 
-let myImage = document.querySelector('img');
+const myImage = document.querySelector('img');
 
 myImage.onclick = function () {
-    let mySrc = myImage.getAttribute('src');
-    switch (mySrc) {
-        case 'images/skyline.jpg':
-            myImage.setAttribute('src', 'images/skyline-gtr.jpg');
-            break;
+   const mySrc = myImage.getAttribute('src');
+   switch (mySrc) {
+      case 'images/skyline.jpg':
+         myImage.setAttribute('src', 'images/skyline-gtr.jpg');
+         break;
 
-        case 'images/skyline-gtr.jpg':
-            myImage.setAttribute('src', 'images/nissan-gtr.jpg');
-            break;
+      case 'images/skyline-gtr.jpg':
+         myImage.setAttribute('src', 'images/nissan-gtr.jpg');
+         break;
 
-        case 'images/nissan-gtr.jpg':
-            myImage.setAttribute('src', 'images/skyline.jpg');
-            break;
-    }
-}
+      case 'images/nissan-gtr.jpg':
+         myImage.setAttribute('src', 'images/skyline.jpg');
+         break;
+   }
+};
